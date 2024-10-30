47d103692de540e3014bc459ad95b8c210575523
Pedro Martins <pedro@pedromartins.com.pt>
2024-10-30 18:45:58 +0000

    First commit

---
+++ src/components/test.js
@@ -0,0 +1,9 @@
+// Create a vanilla Javascript function that sanitizes an html string. The function should remove all html tags from the string and return the sanitized string. The function should be named sanitizeHtml and should take a single argument, htmlString. The function should be exported as a default export.
+
+// Example usage:
+// import sanitizeHtml from './test';
+// const htmlString = '<p>hello world</p>';
+// const sanitizedString = sanitizeHtml(htmlString);
+
+
+
---
+++ src/lib/contentful.js
@@ -0,0 +1,14 @@
+import * as contentful from "contentful";
+
+export const contentfulClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
+
+export async function teamEntries() {
+  const entries = await contentfulClient.getEntries({ content_type: 'team' });
+  return entries.items;
+}
